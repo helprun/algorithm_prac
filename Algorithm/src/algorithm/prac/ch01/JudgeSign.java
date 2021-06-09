33162069c23c35685bc6dedd28470c0ccec45491
pure0320 <helprun@ideatec.co.kr>
2021-06-10 02:58:35 +0900

    알고리즘 입문-조건 판단과 분기

---
+++ Algorithm/src/algorithm/prac/ch01/JudgeSign.java
@@ -0,0 +1,23 @@
+package algorithm.prac.ch01;
+
+import java.util.Scanner;
+
+public class JudgeSign {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요.");
+		int n = scanner.nextInt();
+		
+//		세 갈래로 분기
+//		if(n > 0) System.out.println("이 수는 양수입니다.");
+//		else if(n < 0) System.out.println("이 수는 음수입니다.");
+//		else System.out.println("이 수는 0 입니다.");
+		
+//		네 갈래로 분기한다
+//		if (n == 1) System.out.println("이 수는 1입니다");
+//		else if (n == 2) System.out.println("이 수는 2입니다");
+//		else if (n == 3) System.out.println("이 수는 3입니다");
+//		else ; 실제로는 아무것도 하지 않는 공백문이 있기때문에 네갈래로 분기, 위의 형식과 다른 구조를 가진다
+	}
+}
