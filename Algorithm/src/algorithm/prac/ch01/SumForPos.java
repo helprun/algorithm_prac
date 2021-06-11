c3d7a478cb8b7b5c780c4112e9f6ed3c75778630
helprun <akiratksk@hotmail.com>
2021-06-11 23:47:34 +0900

    알고리즘 입문 사전 판단 반복, 사후 판단 반복

---
+++ Algorithm/src/algorithm/prac/ch01/SumForPos.java
@@ -0,0 +1,42 @@
+package algorithm.prac.ch01;
+
+import java.io.ObjectInputStream.GetField;
+import java.util.Scanner;
+
+public class SumForPos {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n1;
+		int n2;
+		
+//		System.out.println("1부터 n까지 합");
+//		
+//		do {
+//			System.out.println("n의 값 : ");
+//			n = scanner.nextInt();
+//		} while (n <= 0);
+//		
+//		int sum = 0;
+//		
+//		for (int i = 1; i <= n; i++) sum += i;
+//		
+//		System.out.println("1부터 "+ n + " 까지 합 : " +  sum);
+		
+		// 연습문제 Q10
+//		System.out.println("n1 의 값");
+//		n1 = scanner.nextInt();
+//		do {
+//			System.out.println("n2의 값");
+//			n2 = scanner.nextInt();
+//		} while (n2 < n1);
+//		
+//		System.out.println(n2 - n1);
+		
+		System.out.println(getNumLength(135123));
+		
+	}
+	
+	public static int getNumLength(int num) {
+		return String.valueOf(num).length();
+	}
+}
